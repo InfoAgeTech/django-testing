943ee90ea2b4c06e34965e30516773fab87914f0
Troy Grosfield <troy.grosfield@gmail.com>
2013-07-06 11:44:51 -0600

    Initial commit for django_sharing.

---
+++ django_testing/user_utils.py
@@ -0,0 +1,18 @@
+import uuid
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+random_string = lambda len = None: uuid.uuid4().hex[:len or 10]
+
+
+def create_user(username=None, email=None):
+
+    if not username:
+        username = random_string()
+
+    if not email:
+        email = '{0}@{1}.com'.format(random_string(), random_string())
+
+    return User.objects.create_user(username=username,
+                                    email=email)
